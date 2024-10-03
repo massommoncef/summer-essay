273ea920f3cf17b236b0fb7b525607b19a854df4
massoum <massoummoncefpro4@gmail.com>
2024-10-03 01:10:48 +0100

    clock is working on the timer project

--- timer/timer.js
+++ timer/timer.js
@@ -1,20 +1,25 @@
-document.addEventListener("DOMContentLoaded",function(){
 
-    const date = new Date();
-    const hour= date.getHours;
-    const minute= date.getMinutes;
-    const second = date.getSeconds;
+document.addEventListener("DOMContentLoaded", function() {
     const affichage = document.querySelector(".affichage");
-     
-    function getCurrentTime(){
-        const date = new Date();
-        const hour= date.getHours;
-        const minute= date.getMinutes;
-        const second = date.getSeconds;
 
-        affichage.textContent= `${Math.hour}`;
+    function getCurrentTime() {
+        const date = new Date();
+        const hour = date.getHours();
+        const minute = date.getMinutes();
+        const second = date.getSeconds();
+
+        // let currentTime=hour + minute + second;
+        const currentTime = hour.toString().padStart(2,'0')+ ':' + minute.toString().padStart(2,'0')+ ':'  + second.toString().padStart(2,'0') ;
+        // console.log(`${currentTime}`);
+        affichage.textContent= currentTime;
+        
     }
 
-    setInterval(getCurrentTime,100);
+    function timeAffiched(){
+        if(second==0){
+            minute-=1;
+        }
+    }
 
+    setInterval(getCurrentTime, 1000);
 });
